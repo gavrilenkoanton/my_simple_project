21eb28a73a4fbb292788b6990a5f5a8bac07ce23
gavrilenkoanton <59771981+gavrilenkoanton@users.noreply.github.com>
2020-02-25 13:36:07 +0300

    Updates

---
+++ precache-manifest.62a1cba32141c7ffb00e5d9b6289865b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c85a6bab54d5944c71deee14bc1e7c5",
+    "url": "/my_simple_project/index.html"
+  },
+  {
+    "revision": "f8847dff2b04c61334bf",
+    "url": "/my_simple_project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5e7b27d7d1b5e7ce60c0",
+    "url": "/my_simple_project/static/js/2.4f66c214.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my_simple_project/static/js/2.4f66c214.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8847dff2b04c61334bf",
+    "url": "/my_simple_project/static/js/main.1d0f7bc6.chunk.js"
+  },
+  {
+    "revision": "b35707b078dd3ce4d5f1",
+    "url": "/my_simple_project/static/js/runtime-main.858b19d0.js"
+  }
+]);
